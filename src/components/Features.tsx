fc146d75b15e3ffc9f73ab3fdccea67a03bf8965
sahiee-dev <sahir150305@gmail.com>
2026-01-06 23:49:28 +0530

    addition of new files

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 
 const features = [
     {
@@ -82,7 +82,7 @@ const features = [
     }
 ];
 
-const containerVariants = {
+const containerVariants: Variants = {
     hidden: {},
     visible: {
         transition: {
@@ -92,7 +92,7 @@ const containerVariants = {
     },
 };
 
-const itemVariants = {
+const itemVariants: Variants = {
     hidden: {
         opacity: 0,
         y: 20,
